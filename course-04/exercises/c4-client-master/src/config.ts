3188b08de806652cd59174f1a1f3fd9075ae205c
karl <kbessette@ticketaria.com>
2020-12-26 10:36:21 -0500

    Changes due to formation exercises

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'nhmk625tw0'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'pcuq92d3z1'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'test-endpoint.auth0.com',
-  clientId: 'A72UMiQQJzmHQRPpe1dHOyq3EfPvo3Hy',
+  domain: 'kfunk.us.auth0.com',
+  clientId: 'IoqisO5Q5195innGT9BnrSF1baAv2FdP',
   callbackUrl: 'http://localhost:3000/callback'
 }
